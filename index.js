5567d80496ab930cc5b776341df57f878c9cab61
jnhn333 <jnhn333@gmail.com>
2019-09-07 04:28:25 +0900

    3124

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ var server = http.Server(app);
 
 var io = require('socket.io')(server);
 
-app.use(express.static('client'));
+app.use(express.static(__dirname + '/chatting'));
+
+//app.use(express.static('client'));
 
 server.listen(PORT, function() {
   console.log('Chat server running');
 });
 
-app.get('/',function(req, res){  //2
-  res.sendFile(__dirname + '/client.html');
-});
-
+// app.get('/',function(req, res){  //2
+//   res.sendFile(__dirname + '/client.html');
+// });
 io.on('connection', function(socket) {
   socket.on('message', function(msg) {
     io.emit('message', msg);
